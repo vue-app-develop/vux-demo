4ec0c23f0884be56e92738ba0e303452e2c9ee6b
alex <dong,xingbin,alex@gmail.com>
2018-03-30 14:22:03 +0800

    init

--- frontend/src/modules/demo/pages/Message.vue
+++ frontend/src/modules/demo/pages/Message.vue
@@ -4,7 +4,7 @@
     <group label-width="5em" title="消息列表">
       <swipeout class="vux-1px-tb">
         <swipeout-item transition-mode="follow" v-for="notification in notifications"
-                       :key="notification.notificationId" style="border-bottom: #E2DCDC 1px solid">
+                       :key="notification.notificationId" class="item-notification">
           <div slot="right-menu">
             <swipeout-button type="primary" @click.native="updateNotificationReadStatuses(notification.notificationId)">
               设为已读
@@ -13,9 +13,9 @@
               删除
             </swipeout-button>
           </div>
-          <div slot="content" style="height: 42px; padding: 12px; margin-left: 15px;">
-            <p style="font-size: 16px;">{{ notification.title }}</p>
-            <p style="font-size: 14px; color: rgb(102, 102, 102);">{{ notification.content }}</p>
+          <div slot="content" class="item">
+            <p class="item-title">{{ notification.title }}</p>
+            <p class="item-content">{{ notification.content }}</p>
           </div>
         </swipeout-item>
       </swipeout>
@@ -36,8 +36,8 @@
     </group>
     <divider>交互事件</divider>
     <cell>
-    <x-button mini type="primary" onclick="RefreshPortPage();">刷新</x-button>
-    <x-button mini type="warn" onclick="ClosePage();">关闭</x-button>
+      <x-button mini type="primary" onclick="RefreshPortPage();">刷新</x-button>
+      <x-button mini type="warn" onclick="ClosePage();">关闭</x-button>
     </cell>
     <!--<y-footer select="msg">    footer>-->
   </Layout>
@@ -97,13 +97,32 @@
       }, 1000)
       this.getNotifications()
       window.onload = function () {
-        document.getElementById("txtusername").innerText = GetLoginUserId();
-        document.getElementById("txttoken").innerText = GetToken();
+        document.getElementById('txtusername').innerText = GetLoginUserId();
+        document.getElementById('txttoken').innerText = GetToken();
       }
     }
   }
 </script>
 
 <style scoped>
+
+  .item-notification {
+    border-bottom: #E2DCDC 1px solid;
+  }
+
+  .item {
+    height: 42px;
+    padding: 12px;
+    margin-left: 15px;
+  }
+
+  .item-title {
+    font-size: 16px;
+  }
+
+  .item-content {
+    font-size: 14px;
+    color: rgb(102, 102, 102);
+  }
 </style>
 
